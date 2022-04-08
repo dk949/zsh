a05858c18f1b3b3e5efe9df31369b359dfd8cfa7
dk949 <56653556+dk949@users.noreply.github.com>
2022-04-08 18:07:44 +0100

    updated paths

--- path.zsh
+++ path.zsh
@@ -1,12 +1,31 @@
-export PATH=$PATH:~/.local/bin
-export PATH=$PATH:~/.local/share/npm/bin
-export PATH=$PATH:~/.cabal/bin/
+# My executables
+export PATH="$HOME/.local/bin:$PATH"
+
+#npm
+export PATH="$PATH:$XDG_DATA_HOME/npm/bin"
+
+#cabal
+export PATH="$PATH:$HOME/.cabal/bin/"
+
+# stack
 [ -d ~/.stack/programs/x86_64-linux/ ] && ___TMP="$(find ~/.stack/programs/x86_64-linux -maxdepth 1 -type d | sort -r | head -n 1)"
 [ -n "$___TMP" ] && [ -d "$___TMP" ] && export PATH=$PATH:$___TMP/bin
 unset ___TMP
 
 
+export JDK_HOME="/opt/corretto-11.0.14.10.1/"
+
+# Java
+export PATH="$PATH:$JDK_HOME/bin"
+
+# biber
 export PATH="$PATH:/opt/biber-2.16"
+
+# clion
+export PATH="$PATH:/opt/clion-2021.3.3/bin/"
+
+# elm
+export PATH="$PATH:/opt/elm-0.19.1/"
+
+# visit
 export PATH="$PATH:/opt/visit/bin"
-
-
